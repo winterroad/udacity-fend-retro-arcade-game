ab694b4744b7dd237a2dc26e9c51b5951df3244f
winterroad <ullakaija.talvitie@gmail.com>
2018-04-22 02:15:46 +0300

    Feat: Added functionality for when enemy/player crosses the border.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,52 @@
-/*Constant values
-startY (players starty)
-startX (players start x)
-borderLeft (out of bounds x)
-borderRight (out of bounds x)
-borderUp (out of bounds y)
-borderDown (out of bounds y)
-minSpeed (enemy's min speed)
-maxSpeed (enemy's max speed)
-playerStep (how much a player moves by one keypress)
-*/
+'use strict';
+
+/* Game object literal that includes the "rules" (=constants) of the game */
+let game = {
+  minSpeed: 75,
+  maxSpeed: 600,
+  borderLeft: 0,
+  borderRight: 600,
+  borderUp: 0,
+  borderDown: 500,
+  enemyYMin: 100,
+  enemyYMax: 350,
+  winY: 199,
+  winpoints: 199
+}
+
+/* allEnemies array, that will be filled with the enemies*/
+var allEnemies = [];
+allEnemies = createEnemies(5);
+var player = new Player();
 
 // Enemies our player must avoid
+
 function Enemy(speed, posY){
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
   this.speed = speed;
-  this.x = 0;
+  this.x = -200;
   this.y = posY;
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
   this.sprite = 'images/enemy-bug.png';
+}
+
+function createEnemies(num) {
+  for(let i = 0; i < num; i++){
+    let enemy = new Enemy(50, 100);
+    allEnemies.push(enemy);
+  }
+  return allEnemies;
+}
+
+/*when enemy is over the border, randomize new values*/
+function createRandomPosYX(){
+  return [-100, 300];
+}
+
+function didIWin(x,y){
+
 }
 
 // Update the enemy's position, required method for game
@@ -28,49 +55,86 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + (speed * dt);
-    //check for going off canvas, "reset" enemy.
-};
+    if(this.x < 500){
+    this.x = this.x + (this.speed * dt);
+  }
+    //check for going off canvas, "reset" enemy with new x and y inside the borders.
+    if(this.x > 500){
+      [this.x, this.y] = createRandomPosYX();
+    }
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 function Player(){
   //Replace x and y with proper "location" values
-    this.x = 0;
-    this.y = 0;
-    this.sprite = 'images/char-cat-girl.png'
+    this.x = 200;
+    this.y = 400;
+    /*startY (players starty)
+    startX (players start x)*/
+    this.sprite = 'images/char-cat-girl.png';
     //For the "scoreBoard"
     this.lives = 3;
     this.points = 0;
-}
+};
 
-Player.protype.update(){
+Player.prototype.update = function(){
 
-}
+    if (this.x > 450){
+      this.x = 400;
+    }
 
-Player.prototype.render(){
+    else if(this.x < 0){
+      this.x = 0;
+    }
+
+    else if(this.y > 440) {
+      this.y = 440;
+    }
+
+    else if(this.y < -10) {
+      this.y = -10;
+    }
+};
+
+Player.prototype.render  = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-Player.protype.handleInput(key){
+Player.prototype.handleInput  = function(key){
 
+switch(key){
+
+case "left":
+    this.x -= 100;
+    break;
+
+case "right":
+    this.x += 100;
+    break;
+
+case "up":
+    this.y -= 50;
+    break;
+
+case "down":
+    this.y +=50;
+    break;
+
+default:
+    console.log("Something strange happened.");
 }
+};
 
-Player.protype.checkCollisions(){
+Player.prototype.checkCollisions = function(){
   //Compare players "location" to enemies' location on allEnemies array.
-}
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-let player = new Player();
-
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.

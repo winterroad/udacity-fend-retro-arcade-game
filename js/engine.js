dbed5c7e7c0cda1f17cedeb99a54d4622c12d9fc
winterroad <ullakaija.talvitie@gmail.com>
2018-04-29 20:26:42 +0300

    Refactor: corrected to "this" where needed Refactor: corrected lives updated to use Refactor Refactor: correction to collesion detection Refactor: moved checks from update to handleInput Refactor: beautified js and css

--- js/engine.js
+++ js/engine.js
@@ -174,7 +174,8 @@ var Engine = (function(global) {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png',
-        'images/char-cat-girl.png'
+        'images/char-cat-girl.png',
+        'images/Key.png'
     ]);
     Resources.onReady(init);
 
